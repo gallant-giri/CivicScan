42ec2e361266a15dd151c63126bd0a7902d4496e
gallant-giri <girish4936@gmail.com>
2025-06-22 18:31:27 +0530

    updated

--- BrillianBengaluru/urls.py
+++ BrillianBengaluru/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 from homepage.views import custom_logout
 
@@ -15,10 +14,6 @@ urlpatterns = [
     path('logout/', custom_logout, name='logout'),
 ]
 
+# ✅ Serve media files only in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    # 🚨 Add only if you really need catch-all for development
-    urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
